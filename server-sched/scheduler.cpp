ade8e940c0df4ec53165f1a81464f512de24d430
VoidHaruhi <yuyue1218@bupt.edu.cn>
2021-06-06 19:26:40 +0800

    基本完成调度部分

---
+++ server-sched/scheduler.cpp
@@ -0,0 +1,73 @@
+﻿#include "scheduler.h"
+
+Scheduler::Scheduler(){
+    serviceQueue=new ServiceQueue();
+    waitQueue=new WaitQueue();
+    timer=new QTimer();
+    timer->setInterval(1000*TIME_SLOT);
+    connect(timer,&QTimer::timeout,this,&Scheduler::checkTimeout);
+}
+bool Scheduler::setPara(int mode, double *tempLimits, double defaultTargetTemp, double *feeRateTable){
+    this->defaultMode=mode;
+    this->defaultTargetTemp=defaultTargetTemp;
+    memcpy(this->defaultTempLimit,tempLimits,sizeof(defaultTempLimit));
+    memcpy(this->rateFeeTable,feeRateTable,sizeof(rateFeeTable));
+    state=STATE_SET;
+    return true;
+}
+bool Scheduler::startUp(){
+    state=STATE_READY;
+    return true;
+}
+//wait
+bool Scheduler::changeServiceInWaitQueue(QString oldId, QString newId, int fanSpeed, int waitTime){
+    return waitQueue->change(oldId,newId,fanSpeed,waitTime);
+}
+bool Scheduler::modifyWaitQueue(QString roomId, int fanSpeed, int waitTime){
+    return waitQueue->modify(roomId,fanSpeed,waitTime);
+}
+bool Scheduler::addToWaitQueue(QString roomId, int fanSpeed, int waitTime){
+    return waitQueue->add(roomId,fanSpeed,waitTime);
+}
+//service
+bool Scheduler::changeServiceInServiceQueue(QString oldId, QString newId, int fanSpeed, int startTime){
+    return serviceQueue->change(oldId,newId,fanSpeed,startTime);
+}
+bool Scheduler::modifyServiceQueue(QString roomId, int fanSpeed,int startTime){
+    return serviceQueue->modify(roomId,fanSpeed,startTime);
+}
+bool Scheduler::searchRoom(QString roomId){
+    return serviceQueue->exists(roomId);
+}
+bool Scheduler::addToServiceQueue(QString roomId, int fanSpeed, int startTime){
+    return serviceQueue->add(roomId,fanSpeed,startTime);
+}
+void Scheduler::checkTimeout(){
+    QVector<QString> timeouts=waitQueue->checkTimeout(TIME_SLOT);
+    for(QVector<QString>::iterator it=timeouts.begin();it!=timeouts.end();++it){
+        QString id=*it;
+        int speed=waitQueue->getSpeed(id);
+        schedWait(id,speed);
+    }
+}
+QString Scheduler::compareFanSpeed(int fanSpeed){
+    return serviceQueue->compare(fanSpeed);
+}
+
+void Scheduler::schedWait(QString roomId, int fanSpeed){
+    QTime currentTime=QTime::currentTime();
+    int timenow=currentTime.hour()*3600+currentTime.minute()*60+currentTime.second();
+    /*先直接加入服务队列，如果不行则需要进行调度*/
+    if(!addToServiceQueue(roomId,fanSpeed,timenow)){
+        QString res=compareFanSpeed(fanSpeed);
+        if(res=="none"){
+            modifyWaitQueue(roomId,fanSpeed,TIME_SLICE);
+        }else{
+            int newSpeed=serviceQueue->getSpeed(res);
+            changeServiceInWaitQueue(roomId,res,newSpeed,TIME_SLICE);
+            changeServiceInServiceQueue(res,roomId,fanSpeed,timenow);
+            changeFanSpeed(res,0);
+            changeFanSpeed(roomId,fanSpeed);
+        }
+    }
+}
---
+++ server-sched/scheduler.h
@@ -0,0 +1,54 @@
+﻿#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include<QString>
+#include<QTimer>
+#include<QTimerEvent>
+#include<QTime>
+#include<QObject>
+#include "sched_queue.h"
+#define STATE_SET 0
+#define STATE_READY 1
+#define TIME_SLOT 1/*每隔多少时间进行一次超时检查*/
+#define TIME_SLICE 10/*等待时间片*/
+class Scheduler:public QObject{
+    Q_OBJECT
+public:
+    Scheduler();
+    bool changeServiceInWaitQueue(QString oldId,QString newId,int fanSpeed,int waitTime);
+    bool modifyWaitQueue(QString roomId,int fanSpeed,int waitTime);
+    bool searchRoom(QString roomId);
+    bool changeServiceInServiceQueue(QString oldId,QString newId,int fanSpeed,int waitTime);
+    bool modifyServiceQueue(QString roomId,int fanSpeed,int startTime);
+    QString compareFanSpeed(int fanSpeed);
+    bool addToWaitQueue(QString roomId,int fanSpeed,int waitTime);
+    bool addToServiceQueue(QString roomId,int fanSpeed,int startTime);
+    void checkTimeout();
+    /*对超时的等待服务进行调度*/
+    void schedWait(QString roomId,int fanSpeed);
+
+    bool setPara(int mode,double tempLimits[2],double defaultTargetTemp,double feeRateTable[4]);
+    bool startUp();
+
+    bool powerOn(QString roomId,double currentTemp);
+    bool powerOff(QString roomId);
+    bool startAc(int mode,double targetTemp,double rateFeeTable[4],double currentFee,double totalFee);
+    bool changeTargetTemp(QString roomId,double targetTemp);
+    bool changeFanSpeed(QString roomId,int fanSpeed);
+    bool requestTempUp();
+    bool requestRelease(QString roomId);
+
+
+private:
+    int defaultMode;
+    double defaultTargetTemp;
+    double defaultTempLimit[2];
+    double rateFeeTable[4];
+    int state;
+    ServiceQueue *serviceQueue;
+    WaitQueue *waitQueue;
+    QTimer *timer;
+};
+
+
+#endif // SCHEDULER_H
