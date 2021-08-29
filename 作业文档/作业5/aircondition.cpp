b575335c5d35961cad5748ed10c69f3a7bdbc9c2
VoidHaruhi <yuyue1218@bupt.edu.cn>
2021-08-29 19:27:03 +0800

    docs
    docs

---
+++ 作业文档/作业5/aircondition.cpp
@@ -0,0 +1,152 @@
+﻿#include "aircondition.h"
+#include "headers.h"
+AirCondition::AirCondition()
+{
+    mode = 0;   /* 默认制冷 */
+    roomId = -1;
+    token = "";
+
+    initialTemp = 24;
+    currentTemp = 24;
+    targetTemp = 24;
+    lastUpdateTemp = 24;
+
+    fanSpeed = 0;
+    rateFee = rateFeeTable[fanSpeed];
+    totalFee = currentFee = 0;
+
+    idle = true;
+    isStart = false;
+
+    rd.clear();
+}
+
+AirCondition::AirCondition(int workMode, double *rateFeeTable)
+{
+    idle = true;
+    isStart = false;
+
+    mode = workMode;   /* 0-制冷 1-制热 */
+    roomId = -1;
+    token = "";
+
+    initialTemp = 24;
+    currentTemp = 24;
+    targetTemp = 24;
+    lastUpdateTemp = 24;
+
+    /* 费率表由管理员设置 */
+    for(int i=0; i<4; i++){
+        this->rateFeeTable[i] = rateFeeTable[i];
+    }
+
+    fanSpeed = 0;
+    rateFee = rateFeeTable[fanSpeed];
+    totalFee = currentFee = 0;
+
+    rd.clear();
+}
+
+AirCondition::~AirCondition()
+{
+}
+
+void AirCondition::initial(QString id, double nowTemp)
+{
+    roomId = id;
+    currentTemp = nowTemp;
+}
+
+void AirCondition::open(QString systemToken){
+    token = systemToken;
+    idle = false;   /* 该空调已被插卡，未启动 */
+}
+
+void AirCondition::start()
+{
+    initialTemp = currentTemp;
+    lastUpdateTemp = currentTemp;
+    fanSpeed = 1;   /* 默认是低速风 */
+    rateFee = rateFeeTable[fanSpeed];
+    lastRequestTime = getCurrentTime();
+    lastRequestFee = 0;
+    isStart = true;
+}
+
+
+void AirCondition::changeFanSpeed(int spd)
+{
+    updateFee();
+    addRD();
+    fanSpeed = spd;
+    rateFee = rateFeeTable[fanSpeed];
+}
+
+void AirCondition::changeTargetTemp(double tmp)
+{
+    targetTemp = tmp;
+}
+
+void AirCondition::setCurrentTemp(double tmp)
+{
+    currentTemp = tmp;
+}
+
+double AirCondition::getNewFee()
+{
+    if(mode == 0){  /* 制冷模式温度变化是负数 */
+        return (lastUpdateTemp - currentTemp) * rateFee;
+    }
+    else if(mode == 1){
+        return (currentTemp - lastUpdateTemp) * rateFee;
+    }
+}
+
+double AirCondition::updateFee()
+{
+    // 更新当前累计消费*/
+    double fee = getNewFee();
+    totalFee += fee;
+    currentFee += fee;
+    lastUpdateTemp = currentTemp;
+    return fee;
+}
+
+void AirCondition::addRD()
+{
+    RecordDetail rdItem;
+    rdItem.roomId = this->roomId;
+    rdItem.requestTime = this->lastRequestTime;
+    lastRequestTime = getCurrentTime();
+    rdItem.requestDuration = lastRequestTime - rdItem.requestTime;
+    rdItem.fanSpeed = this->fanSpeed;
+    rdItem.feeRate = this->rateFee;
+    rdItem.fee = currentFee - lastRequestFee;
+    lastRequestFee = currentFee;
+    rd.append(rdItem);
+}
+
+double AirCondition::getTotalFee()
+{
+    return totalFee;
+}
+
+int AirCondition::getCurrentTime()
+{
+    QTime current_time = QTime::currentTime();
+    int nowTime = (current_time.hour()*3600 + current_time.minute()*60 + current_time.second());
+    return nowTime;
+}
+
+void AirCondition::end()
+{
+    updateFee();
+    addRD();
+    isStart = false;
+    fanSpeed = 0;
+    rateFee = rateFeeTable[fanSpeed];
+	currentFee = 0;
+
+}
+
+
---
+++ 作业文档/作业5/aircondition.h
@@ -0,0 +1,71 @@
+﻿#ifndef AIRCONDITION_H
+#define AIRCONDITION_H
+
+#include <QString>
+#include <QTime>
+#include <QList>
+#include "receptionController.h"
+
+#define DISCONNECTED 0
+#define CONNECTED    1  /* 也是idle状态，就是客房连接成功，但是未入住宾客 */
+#define CHECKED      2  /* 非idle状态，该房间有旅客入住 */
+
+class AirCondition
+{
+public:
+
+    bool idle;      /* 无人入住为True */
+    bool isStart;   /* 当前是否工作(开机) */
+    int mode;       /* 模式 0-制冷 1-制热，默认为制冷 */
+    QString roomId;
+    QString token;
+
+    int fanSpeed;   /* 风速wind */
+    double rateFeeTable[4] = {0, 0.5, 1, 2}; /* 费率表 */
+    double rateFee; /* 费率 */
+
+    double initialTemp; /* 初始温度 */
+    double currentTemp; /* 当前温度 */
+    double targetTemp;  /* 目标温度 */
+    double lastUpdateTemp; /* 上次更新风速时的温度 */
+
+    double currentFee;  /* 本次开机的消费 */
+    double totalFee;    /* 该空调总消费 */
+
+    QList<RecordDetail> rd; /* 该空调声明周期的详单 */
+    int lastRequestTime;   /* 上次详单更新的时间 */
+    double lastRequestFee;    /* 上次详单更新的消费 */
+
+    AirCondition();
+    AirCondition(int workMode, double *rateFeeTable);
+    ~AirCondition();
+
+    /* initial是客户端连接但未开房时进行的操作 */
+    void initial(QString id, double nowTemp);
+    /* open是开房操作，收到管理员开房通知后的操作 */
+    void open(QString systemToken);
+
+    /* 空调开机 */
+    void start();
+    /* 这里只是接收客户端请求后，更新空调的目标温度和风速 */
+    void changeTargetTemp(double tmp);
+    void changeFanSpeed(int spd);
+    /* 用于帮助调度对象更新当前温度 */
+    void setCurrentTemp(double tmp);
+    /* 更新当前温度和累计消费 */
+    double updateFee();
+    /* 新产生的费用 */
+    double getNewFee();
+    /* 增加一条详单记录 */
+    void addRD();
+    /* 获得消费总费用 */
+    double getTotalFee();
+    /* 获得当前时间的秒数 */
+    int getCurrentTime();
+    /* 空调关机 */
+    void end();
+
+
+};
+
+#endif // AIRCONDITION_H
